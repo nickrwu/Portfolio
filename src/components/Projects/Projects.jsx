1f55e44bba460160b526a68dc578b33944c7d397
Nick Wu <raidenmx@yahoo.com>
2020-09-26 17:54:27 -0400

    Edited buttons and header styling

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -52,7 +52,7 @@ const Projects = () => {
                       <a
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="cta-btn cta-btn--hero"
+                        className="cta-btn cta-btn--project"
                         href={url || '#!'}
                       >
                         See Live
