09ee84bf585f2b2f2bebc684cb41fdad8b6539f1
Nick Wu <raidenmx@yahoo.com>
2020-11-09 21:41:34 -0500

    Footer Icons added, hero font resizing

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -27,9 +27,9 @@ const Header = () => {
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
             {title || 'Hi, my name is'}{' '}
-            <span className="text-color-title">{name || 'Your Name'}</span>
+            <span className="text-color-title">{name || 'Nicholas Wu'}</span>
             <br />
-            <span className="hero-subtitle">{subtitle || "I'm the Unknown Developer."}</span>
+            <span className="hero-subtitle">{subtitle || 'NYU Stern Student'}</span>
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
