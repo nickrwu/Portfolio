2d19a2487060e6ff3130b5588793b4ea892b0a16
Nick Wu <nrw9167@nyu.edu>
2020-12-04 18:07:51 -0500

    Edit metadata

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,14 +13,15 @@ export default () => {
       <Helmet>
         <meta charSet="utf-8" />
         <title>{title || 'Nicholas Wu'}</title>
-        <html lang={lang || 'en'} prefix="og: http://ogp.me/ns#"/>
+        <html lang={lang || 'en'} prefix="og: http://ogp.me/ns#" />
         <meta name="description" content={description || 'Portfolio'} />
 
         {/* OGP Metadata */}
-        <meta property="og:title" content={title || 'Nicholas Wu'}/>
-        <meta property="og:description" content={description || 'Portfolio'}/>
-        <meta name = "image" property="og:image" content="src/images/featured.jpg"/>
-        <meta property="og:url" content="https://nickrwu.studio"/>
+        <meta property="og:title" content={title || 'Nicholas Wu'} />
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content="Nicholas Wu Project Portfolio" />
+        <meta name="image" property="og:image" content="../images/featured.jpg" />
+        <meta property="og:url" content="https://nickrwu.studio" />
       </Helmet>
       <App />
     </>
