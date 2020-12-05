936a4d43d2683938d1703d9f6955d8e79681ec2f
Nick Wu <nrw9167@nyu.edu>
2020-12-05 11:06:39 -0500

    Update information

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -29,7 +29,7 @@ const Header = () => {
             {title || 'Hi, my name is'}{' '}
             <span className="text-color-title">{name || 'Nicholas Wu'}</span>
             <br />
-            <span className="hero-subtitle">{subtitle || 'NYU Stern Student'}</span>
+            <span className="hero-subtitle">{subtitle || 'NYU Stern'}</span>
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
